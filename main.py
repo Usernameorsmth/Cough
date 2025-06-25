24c1b2268bab19c0e3c718f5106e3e1b99f86494
Usernameorsmth <simidarawilliams@gmial.com>
2025-06-25 19:09:24 +0100

    cry

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-# Zip elements of two lists
-s1 = [2, 3, 1]
-s2 = ['b', 'a', 'c']
-s3 = list(zip(s1, s2))
-print(s3, '\n')
+# Python program to demonstrate
+# exit()
 
-# Zip elements of two lists
-# Print elements one by one, but elements of 2nd list will be in reverse order
-list1 = [10, 20, 30, 40]
-list2 = [100, 200, 300, 400]
-
-for x, y in zip(list1, list2[::-1]):
-    print(x, y)
-
-# Zip into dictionary
-stocks = ['reliance', 'infosys', 'tcs']
-prices = [2175, 1127, 2750]
-
-new_dict = {stocks: prices for stocks, prices in zip(stocks, prices)}
-print('\n{}'.format(new_dict))
+for i in range(10):
+    # If the value of i becomes
+    # 5 then the program is forced
+    # to exit
+    if i == 5:
+        # prints the exit message
+        print(exit) # Note: This will print the exit function object, not a message
+        exit()
+    print(i)
